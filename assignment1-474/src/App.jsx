63acb9f8110fd4f6f707d15b40561284e9a595b9
Siya Sharma <siyasharma@Siyas-MacBook-Pro.local>
2022-01-20 14:53:48 -0800

    add data js file

--- assignment1-474/src/App.jsx
+++ assignment1-474/src/App.jsx
@@ -1,17 +1,26 @@
 import { scaleLinear, scaleBand, extent, line, symbol, csv } from "d3";
 import { AxisLeft, AxisBottom } from "@visx/axis";
 import { uniq } from "lodash";
+import crimes from "./hatecrimes";
 import './App.css'
 
 function App() {
+  const chartSize = 500;
+  const margin = 30;
+  const motivation = uniq(crimes.motivation);  
 
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <h1>Hate Crimes in San Diego</h1>
+      <svg width={chartSize} height={chartSize} style={{ border: "2px solid black" }}>
+      
+    
+
+      </svg>
+      
     </div>
-  )
+  );
+  
 }
 
 export default App
