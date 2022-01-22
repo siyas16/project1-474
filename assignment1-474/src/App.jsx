a3564f3d7e82cbac7eeb7ba4f87b889f76b38573
Siya Sharma <siyasharma@Siyas-MacBook-Pro.local>
2022-01-22 14:47:56 -0800

    final code

--- assignment1-474/src/App.jsx
+++ assignment1-474/src/App.jsx
@@ -7,7 +7,6 @@ import "./App.css";
 function App() {
   const chartSize = 600;
   const margin = 10;
-  const motivation = uniq(crimes.motivation);
   const crimesTotal = {};
   const allYears = [];
   const motivationTotal = {};
@@ -42,9 +41,17 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Hate Crimes in San Diego</h1>
+      <h1>How have hate crime patterns changed in San Diego overtime?</h1>
+      <h2>
+        What proportion of these hate crimes have been racially motivated?
+      </h2>
+
       <svg width={chartSize} height={chartSize}>
-        <AxisLeft strokeWidth={0} left="35" scale={_scaleY} />
+        <AxisLeft strokeWidth={0} left="45" scale={_scaleY} />
+
+        <text x="-300" y="13" transform="rotate(-90)" fontSize={15}>
+          Number of Crimes
+        </text>
 
         {allYears.map((num, i) => {
           const rectHeight = 10 * crimesTotal[num];
@@ -81,21 +88,83 @@ function App() {
             </text>
           );
         })}
+
+        <text x={150} y={550}>
+          Year
+        </text>
+
+        <rect x={270} y={20} width={20} height={10} fill={"blue"} />
+
+        <text x={300} y={30}>
+          Racially Motivated Crimes
+        </text>
+
+        <rect x={270} y={50} width={20} height={10} fill={"black"} />
+
+        <text x={300} y={60}>
+          Crimes with Other Motivations
+        </text>
       </svg>
 
-      <p>
-        To answer my question, the visualization I chose to create is a bar
-        chart.
-      </p>
+      <svg width={chartSize} height={chartSize}>
+        <text x={0} y={80} fontSize={15}>
+          <tspan x="0" y="45">
+            To answer my question, the visualization I chose to create is a bar
+            chart with
+          </tspan>
+          <tspan x="0" y="65">
+            columns. Bar charts are good for displaying magnitude. Usually, they
+          </tspan>
+          <tspan x="0" y="85">
+            show a "counted" number. I worked with 3 variables: year, motivation
+            of the crime,
+          </tspan>
+          <tspan x="0" y="105">
+            and number of crimes. Number of crimes is a counted number, so it is
+            good
+          </tspan>
+          <tspan x="0" y="125">
+            to display on a bar chart. Additionally, I wanted to showcase a size
+            comparison
+          </tspan>
+          <tspan x="0" y="145">
+            (difference in amount of hate crimes overtime.) and bar charts are
+            used for such
+          </tspan>
+          <tspan x="0" y="165">
+            comparisons.
+          </tspan>
+          <tspan x="0" y="205">
+            I used different colors for the bars, blue and black, to
+            differentiate between
+          </tspan>
+          <tspan x="0" y="225">
+            the different hate crime motivations. I made the left axis scale go
+            up
+          </tspan>
+          <tspan x="0" y="245">
+            by 5 so that the bar heights were easy to read. Also, I created a
+            count variable
+          </tspan>
+          <tspan x="0" y="265">
+            to count up how many crimes happened each year for each motivation
+            category defined.
+          </tspan>
+          <tspan x="0" y="285">
+            All these decisions were necessary to communicate the story I wanted
+            to tell:
+          </tspan>
+          <tspan x="0" y="305">
+            the variation in number of hate crimes per year and variation in
+            racially motivated
+          </tspan>
+          <tspan x="0" y="325">
+            hate crimes per year.
+          </tspan>
+        </text>
+      </svg>
     </div>
   );
 }
 
 export default App;
-
-/*<svg width={chartSize} height={chartSize}>
-        <text x={0} y={20} fontSize={20}>
-          To answer my question, the visualization I chose to create is a bar
-          chart.
-        </text>
-      </svg>*/
